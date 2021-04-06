797b699bf9479fdbed48b06deaf29d5a2d7e2cd5
Lyubov Myetolkina <luba150278@gmail.com>
2021-04-06 15:42:45 +0300

    start  use redux

---
+++ src/components/PostForm.js
@@ -0,0 +1,51 @@
+import { Component } from "react";
+
+export default class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+  }
+  submitHandler = (event) => {
+    event.preventDefault();
+    const { title } = this.state;
+    const newPost = {
+      title,
+      id: Date.now().toString(),
+    };
+
+    console.log(newPost);
+    this.setState({ title: "" });
+  };
+
+  changeInputHandler = (event) => {
+    event.persist();
+    this.setState((prev) => ({
+      ...prev,
+      ...{ [event.target.name]: event.target.value },
+    }));
+  };
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Заголовок поста
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeInputHandler}
+          />
+        </div>
+        <button className="btn btn-success" type="submit">
+          Создать пост
+        </button>
+      </form>
+    );
+  }
+}
